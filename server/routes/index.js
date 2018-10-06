727d3507b70eef7b77909f255fe7cda435f5bc29
pputra <mynameisyoga@live.com>
2018-10-06 14:10:33 +0700

    add user login and register

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const UserController = require('../controllers/userController');
+const ActivityController = require('../controllers/activityController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('home');
 });
+
+router.post('/register', UserController.register);
+
+router.post('/login', UserController.login);
+
+
+
+//router.post('/activity/add', ActivityController.add);
+
+//router.get('/:id', UserController.findById);
+
 
 module.exports = router;
